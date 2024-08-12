acce84556c986f90a7c94807bba0864204652914
Ahmed Yousry <ahmedu3helal@gmail.com>
2024-08-12 17:40:16 +0300

    Finish Not Found Component

--- DeltaSoft-Task/src/app/app.routes.ts
+++ DeltaSoft-Task/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './Components/home/home.component';
+import { NotFoundComponent } from './Components/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: "home", component: HomeComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', component: NotFoundComponent}
+];
+
